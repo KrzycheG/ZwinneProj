aabe6fe9794d6a4f1b9ab7e235e050f817c56029
KrzycheG <kpstrowski@gmail.com>
2021-10-11 20:17:50 +0200

    Zapisywanie ksiazek do json

--- src/main/java/com/example/demo/ZwinneProjektApplication.java
+++ src/main/java/com/example/demo/ZwinneProjektApplication.java
@@ -1,15 +1,9 @@
 package com.example.demo;
-import com.example.demo.book.book;
-import org.apache.el.parser.AstIdentifier;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class ZwinneProjektApplication {
 
 	public static void main(String[] args) {
@@ -17,17 +11,6 @@ public class ZwinneProjektApplication {
 		SpringApplication.run(ZwinneProjektApplication.class, args);
 
 	}
-	@GetMapping
-
-	public List<book> books(){
-		return List.of(
-			new book(
-				"Potop",
-				25,5
-			)
-		);
-	}
-
 
 
 }
---
+++ src/main/java/com/example/demo/book/bookController.java
@@ -0,0 +1,23 @@
+package com.example.demo.book;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="api/v1/book")
+public class bookController {
+
+    @GetMapping
+    public List<book> getbooks(){
+        return List.of(
+                new book(
+                        "Potop",
+                        25,5
+                )
+        );
+    }
+
+}
