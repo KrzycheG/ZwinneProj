11dc1b1acc72626066ffe06a7e60d20963c863dd
KrzycheG <kpstrowski@gmail.com>
2021-10-11 19:18:49 +0200

    Pierwszy wrzut

---
+++ src/main/java/com/example/demo/ZwinneProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwinneProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwinneProjektApplication.class, args);
+	}
+
+}
