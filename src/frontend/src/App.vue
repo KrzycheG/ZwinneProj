8c22b0c83fcde93bec4489fdfa3d959d3ae2bbc8
Puzjeh <puzmarcin@gmail.com>
2021-10-15 22:31:56 +0200

    zrobilem komponent kafelka przedmiotu, komentarze w kodzie

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  gownosdffdsf
+  <header>
+    NAZWA SKLEPU KATEGORIE KOSZYK
+  </header>
+  <div>
+    <product-card />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ProductCard from '@/components/ProductCard'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { ProductCard }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/frontend/src/components/ProductCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <article class="Product_Card" id="app">
+    <!--uniwersalna scieżka do .jpg brak zdjecia, ktore bedzie wyswietlane w przypadku braku .jpg w bazie danego produktu -->
+    <img class="product_image" src="../assets/brak_zdjecia.jpg" >
+    <!--nazwa produktu docelowo bedzie zaciagana z bazy -->
+    <p class="Product_name">Nazwa produktu</p>
+    <!--cena produktu docelowo bedzie zaciagana z bazy -->
+    <p class="Product_price">Cena produktu</p>
+    <img class="Add_to_cart" src="../assets/cart.png" >
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard'
+}
+</script>
+
+<style lang="css" scoped>
+  .product_image{
+    width: 150px;
+    height: 200px;
+    margin: 3px auto;
+    display: list-item;
+    border: 3px solid grey;
+  }
+  .Product_Card{
+    border: solid 5px grey;
+    width: 250px;
+    height: 350px;
+    display: block;
+  }
+  .Product_name{
+    margin-left: 10px;
+  }
+  .Product_price{
+    margin-left: 10px;
+  }
+  .Add_to_cart{
+    width: 40px;
+    height: 40px;
+    background-color: grey;
+    display: block;
+    margin: auto;
+  }
+
+</style>
