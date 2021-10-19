f76d769a70b97dfeebe5a2990172a3b31eea345f
szymonnoras <saronomysz@o2.pl>
2021-10-19 19:37:49 +0200

    uzupełnienie bazy

--- src/main/java/com/example/demo/book/bookConfig.java
+++ src/main/java/com/example/demo/book/bookConfig.java
@@ -14,15 +14,60 @@ public class bookConfig {
             bookRepository repository){
 
         return args -> {
-            book potop = new book(
-                    1L,
-                    "Potop","x","x",1,
-                    25,5 ,1
+            book hobbit = new book(
+                    11L,
+                    "Hobbit",
+                    "J. R. R. Tolkien",
+                    "George Allen & Unwin",
+                    1937,
+                    29.99,
+                    45 ,
+                    1
+            );
+            book it = new book(
+                    22L,
+                    "IT",
+                    "Stephen King",
+                    "Viking",
+                    1986,
+                    39.99,
+                    25 ,
+                    2
+            );
+            book ToE = new book(
+                    33L,
+                    "The Theory of Everything: The Origin and Fate of the Universe",
+                    "Stephen Hawking",
+                    "Jaico Publishing House",
+                    2002,
+                    79.99,
+                    125 ,
+                    3
+            );
+            book DVC = new book(
+                    44L,
+                    "The Da Vinci Code",
+                    "Dan Brown",
+                    "Doubleday",
+                    2003,
+                    69.99,
+                    72 ,
+                    4
+            );
+            book FiOS = new book(
+                    55L,
+                    "The Fault in Our Stars",
+                    "John Green",
+                    "Dutton Books",
+                    2012,
+                    39.99,
+                    12 ,
+                    5
             );
 
 
             repository.saveAll(
-                    List.of(potop) // dopisac reszte po przecinku
+                    List.of(hobbit,it,ToE,DVC,FiOS) // dopisac reszte po przecinku
             );
         };
     }
