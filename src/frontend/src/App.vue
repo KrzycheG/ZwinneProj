38e93f5c26ec5cf04b12627bee79dd643eeaa2a8
Puzjeh <puzmarcin@gmail.com>
2021-10-20 22:22:30 +0200

    zrobienie wyświetania listy wszystkich produktow na stronie z ich wartosciami

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -2,17 +2,35 @@
   <header>
     NAZWA SKLEPU KATEGORIE KOSZYK
   </header>
-  <div>
-    <product-card />
+  <div class="productList">
+    <product-card v-for="product of products" :key="product.id" :product="product"/>
   </div>
 </template>
 
 <script>
 import ProductCard from '@/components/ProductCard'
+import {
+  useProduct
+} from '@/composables/useProduct'
+
 export default {
-  components: { ProductCard }
+  components: { ProductCard },
+  setup () {
+    const {
+      getProducts, products
+    } = useProduct()
+    getProducts()
+    return {
+      products
+    }
+  }
 }
 </script>
 
 <style>
+.productList {
+  display: flex;
+  padding: 150px;
+  justify-content: space-between;
+}
 </style>
--- src/frontend/src/components/ProductCard.vue
+++ src/frontend/src/components/ProductCard.vue
@@ -1,42 +1,46 @@
 <template>
   <article class="Product_Card" id="app">
     <!--uniwersalna scieżka do .jpg brak zdjecia, ktore bedzie wyswietlane w przypadku braku .jpg w bazie danego produktu -->
-    <img class="product_image" src="../assets/brak_zdjecia.jpg" >
-    <!--nazwa produktu docelowo bedzie zaciagana z bazy -->
-    <p class="Product_name">Nazwa produktu</p>
-    <!--cena produktu docelowo bedzie zaciagana z bazy -->
-    <p class="Product_price">Cena produktu</p>
-    <img class="Add_to_cart" src="../assets/cart.png" >
+    <img class="product_image" src="../assets/brak_zdjecia.jpg">
+    <p class="Product_name">{{ product.title }}</p>
+    <div><span class="Product_price">{{ product.price }}zł</span>
+      <button><img class="Add_to_cart" src="../assets/cart.png"></button>
+    </div>
   </article>
 </template>
 
 <script>
 export default {
-  name: 'ProductCard'
+  name: 'ProductCard',
+  props: ['product']
 }
 </script>
 
 <style lang="css" scoped>
-.product_image{
+.product_image {
   width: 150px;
   height: 200px;
   margin: 3px auto;
   display: list-item;
   border: 3px solid grey;
 }
-.Product_Card{
+
+.Product_Card {
   border: solid 5px grey;
   width: 250px;
   height: 350px;
   display: block;
 }
-.Product_name{
+
+.Product_name {
   margin-left: 10px;
 }
-.Product_price{
+
+.Product_price {
   margin-left: 10px;
 }
-.Add_to_cart{
+
+.Add_to_cart {
   width: 40px;
   height: 40px;
   background-color: grey;
