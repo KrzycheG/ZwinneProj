a1b86614bb27d13b2fc2a1aeebd9a5787cc880c6
szymonnoras <saronomysz@o2.pl>
2021-11-12 17:53:22 +0100

    stworzony generator pdf

---
+++ src/main/java/com/example/demo/pdfService/pdfGeneratorService.java
@@ -0,0 +1,33 @@
+package com.example.demo.pdfService;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Service
+public class pdfGeneratorService {
+    public void export(HttpServletResponse response) throws IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document,response.getOutputStream());
+
+        document.open();
+        Font fontTitle= FontFactory.getFont(FontFactory.TIMES);
+        fontTitle.setSize(18);
+
+        Paragraph paragraph = new Paragraph("This is a title.",fontTitle);
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph.setSize(12);
+
+        Paragraph paragraph2 = new Paragraph("This is a paragraph.",fontParagraph);
+        paragraph2.setAlignment(Paragraph.ALIGN_LEFT);
+
+        document.add(paragraph);
+        document.add(paragraph2);
+        document.close();
+    }
+}
