e1479a07c37f7b016829dcf211ccf60c269d49bb
Puzjeh <puzmarcin@gmail.com>
2021-11-15 22:52:51 +0100

    dzialajacy formularz danych do wysylki + przycisk w koszyku

---
+++ src/frontend/src/components/Checkout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="modal fade" id="checkout" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Dane adresowe</h5>
+        </div>
+        <div class="modal-body">
+          <CheckoutForm></CheckoutForm>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CheckoutForm from '@/components/CheckoutForm.vue'
+export default {
+  components: { CheckoutForm }
+}
+</script>
