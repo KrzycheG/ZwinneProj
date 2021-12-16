3bc0bb289ee745f64130c298973db5d776c160be
Puzjeh <puzmarcin@gmail.com>
2021-12-17 00:20:53 +0100

    naprawa quantity + css

--- src/frontend/src/composables/useCartProduct.js
+++ src/frontend/src/composables/useCartProduct.js
@@ -9,27 +9,23 @@ const state = ref({
 })
 export const useCartProduct = () => {
   const getProducts = async () => {
-    const response = await axios.get('http://localhost:8080//api/v1/cart/cartBookList')
-    state.value.products = response.data
-    return response
+    const products = await axios.get('http://localhost:8080//api/v1/cart/cartBookList').then(response => response.data)
+    console.log(products)
+    const quantities = await Promise.all(products.map(product => axios.get('http://localhost:8080/api/v1/cart/quantity/' + product.id).then(response => response.data[0])))
+    console.log(quantities)
+    state.value.products = products.map((product, index) => ({ ...product, quantity: quantities[index] }))
   }
   const getsum = async () => {
     const response = await axios.get('http://localhost:8080//api/v1/cart/suma')
     state.value.sum = response.data[0]
     return response
   }
-  const getQuantity = async () => {
-    const response = await axios.get('http://localhost:8080//api/v1/cart')
-    state.value.quantity = response.data
-    return response
-  }
+  const total = computed(() => state.value.products.reduce((sum, product) => sum + product.quantity, 0))
   return {
     getProducts,
     products: computed(() => state.value.products),
     getsum,
     sum: computed(() => state.value.sum),
-    totalItems: computed(() => state.value.products.length),
-    getQuantity,
-    quantity: computed(() => state.value.quantity)
+    totalItems: total
   }
 }
